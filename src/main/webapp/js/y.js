1a092779edb553b2bd051fbe7e5c3c69e4e7a947
Jacek <jacek@kiquidkit.com>
2016-02-03 13:55:48 +0100

    directive

--- src/main/webapp/js/y.js
+++ src/main/webapp/js/y.js
@@ -7,3 +7,21 @@ angular.module("myApp")
     .controller("SecondController", function($scope) {
 
     });
+
+//angular.module("myApp")
+//    .directive("myDirective", function() {
+//        return {
+//            restrict: 'A',
+//            scope: true
+//        }
+//    });
+
+angular.module("myApp")
+    .controller("MainController", function($scope) {})
+    .directive("myDirective", function() {
+        return {
+            restrict: 'A',
+            scope: {},
+            template: '<div>Inside myDirective {{ myProperty }}</div>'
+        };
+    });
