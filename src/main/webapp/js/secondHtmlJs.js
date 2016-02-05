94e69f53815954c486f1df794e7f5a66a11286d6
Jacek <jacek@kiquidkit.com>
2016-02-05 08:30:40 +0100

    ng-controller anonymous scope

--- src/main/webapp/js/secondHtmlJs.js
+++ src/main/webapp/js/secondHtmlJs.js
@@ -9,3 +9,20 @@ angular.module("myApp", [])
             template: '<div class="sidebox"> <div class="content"> <h2>{{ title }}</h2> <span class="content" ng-transclude></span> </div> </div>'
         };
     });
+
+angular.module("myApp")
+    .controller("MainControllerNoScope", function() {
+        this.name = "Ari";
+    });
+
+angular.module("myApp")
+    .directive("myDirectiveAnonymousController", function() {
+        return {
+            restrict: 'A',
+            template: '<h4>{{ myController.msg }}</h4>',
+            controllerAs: 'myController',
+            controller: function() {
+                this.msg = "Hello World"
+            }
+        };
+    });
